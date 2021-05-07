7e797c6380920caed7214d08e2bf73a1bd605ea5
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-07 08:45:14 +0000

    Added city view

---
+++ api/v1/views/cities.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" Cities view module """
+from models.amenity import Amenity
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask import make_response
+
+
+@app_views.route('/states/<state_id>/cities', strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE'])
+def cities_get_delete(state_id=None, city_id=None):
+    """ Cities route """
+    if state_id is not None:
+        state = storage.get(State, state_id)
+        # Return 404 if state_id not exists
+        if state is None:
+            abort(404)
+        city_list = []
+        for city in state.cities:
+            city_list.append(city.to_dict())
+        return jsonify(city_list)
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(city.to_dict())
+    elif request.method == 'DELETE':
+        storage.delete(city)
+        storage.save()
+        return jsonify({})
+
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
+def city_post(state_id):
+    """Cities POST Route"""
+    try:
+        obj = request.get_json()
+    except Exception:
+        abort(400, 'Not a JSON')
+    if 'name' not in obj:
+        abort(400, 'Missing name')
+    obj['state_id'] = state_id
+    city = City(**obj)
+    storage.new(city)
+    storage.save()
+    return make_response(jsonify(city.to_dict()), 201)
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'])
+def cities_put(city_id=None):
+    """ Cities PUT route """
+    ignore_keys = ['updated_at', 'created_at', 'id', 'state_id']
+    try:
+        obj = request.get_json()
+    except Exception:
+        abort(400, 'Not a JSON')
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    for k, v in obj.items():
+        if k not in ignore_keys:
+            setattr(city, k, v)
+    storage.save()
+    return make_response(jsonify(city.to_dict()), 200)
