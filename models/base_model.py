49fa50b151f6d1d3fa9c0137136e4161c2a5c48a
7honarias <7honarias@gmail.com>
2021-05-09 01:24:37 +0000

    [UPDATE] - removed has attr

--- models/base_model.py
+++ models/base_model.py
@@ -56,8 +56,6 @@ class BaseModel:
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
-        if hasattr(self, 'password') and models.storage_t == 'db':
-            self.password = md5(self.password).hexdigest()
         models.storage.new(self)
         models.storage.save()
 
